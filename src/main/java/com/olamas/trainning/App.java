d6a49dbd85ed14b6aafa0a5a8cd577cc092ee6fb
Osvaldo Lamas <osvaldolamas@gmail.com>
2018-01-03 22:55:05 -0300

    Initial settings

---
+++ src/main/java/com/olamas/trainning/App.java
@@ -0,0 +1,13 @@
+package com.olamas.trainning;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
